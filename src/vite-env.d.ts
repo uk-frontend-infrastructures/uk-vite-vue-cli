cba37c3c284ff407a8e1feac741873e8fc222445
cq-zz <2583983764@qq.com>
2023-05-31 10:23:49 +0800

    普通修改

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,5 @@ declare module '*.vue' {
 interface Window {
 	_GlobalConfig: {
 		_API_ROOT: string;
-		_ROOT_URL: string;
 	};
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import htmlPlugin, { Options as HtmlPluginOptions } from 'vite-plugin-html-config';
 import path from 'path';
 
-const _BASE_URL = 'TEST';
+const _BASE_URL = 'test';
 
-export default defineConfig(mode => {
+export default defineConfig(({ mode }) => {
+	if (mode === 'development') {
+		//
+	}
+
 	const htmlPluginOption: HtmlPluginOptions = {
 		favicon: '/favicon.ico',
 		// headScripts: [{ src: `./config.js` }],
@@ -16,13 +20,21 @@ export default defineConfig(mode => {
 	return {
 		plugins: [vue(), htmlPlugin(htmlPluginOption)],
 		resolve: {
-			alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }] // 添加src文件夹'@'别名
+			alias: {
+				'@': path.resolve(__dirname, './src')
+			} // 添加src文件夹'@'别名
 		},
 		server: {
 			host: '0.0.0.0',
 			port: 9000, // 开发服务器端口号
 			open: true, // 默认打开
-			hmr: true // 开启热更新
+			hmr: true, // 开启热更新
+			// 开发环境-服务代理
+			proxy: {
+				// '/api': {
+				// 	target: 'http://xxx.xxx.xxx'
+				// }
+			}
 		},
 		base: _BASE_URL,
 		build: {
