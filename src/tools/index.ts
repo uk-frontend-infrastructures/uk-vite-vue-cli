76acca7d94a44f740edb6b62eb89260f4b0de6c2
cq-zz <2583983764@qq.com>
2023-06-06 12:08:33 +0800

    按钮权限

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -21,3 +21,32 @@ export const getMenuAuth = (menus: MenuType[], curMenu: string): boolean => {
 	}
 	return auth;
 };
+
+/**
+ * 获取按钮权限
+ * @param menus 所有菜单
+ * @param curMenu 当前菜单key
+ * @param btnKey 当前按钮key
+ */
+export const getBtnAuth = (menus: MenuType[], curMenu: string, btnKey: string): boolean => {
+	let auth = false;
+	for (let i = 0; i < menus.length; i++) {
+		const menu: MenuType = menus[i];
+		if (menu.key === curMenu) {
+			if (menu.status && menu.buttons && menu.buttons.length) {
+				for (let j = 0; j < menu.buttons.length; j++) {
+					if (menu.buttons[j].key === btnKey) {
+						return menu.buttons[j].status;
+					}
+				}
+			} else {
+				return false;
+			}
+		} else {
+			if (menu.children && menu.children.length) {
+				auth = getBtnAuth(menu.children, curMenu, btnKey);
+			}
+		}
+	}
+	return auth;
+};
